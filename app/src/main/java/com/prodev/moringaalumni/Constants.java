aef899e375f9b3bbd559ddb6f65257cdb828466b
Njoki Kamau <njokifaith61@gmail.com>
2020-08-25 13:20:51 +0300

    added firebase dependencies

---
+++ app/src/main/java/com/prodev/moringaalumni/Constants.java
@@ -0,0 +1,7 @@
+package com.prodev.moringaalumni;
+
+public class Constants {
+
+    public static final String FIREBASE_CHILD_SEARCHED_USER = "searchedUser";
+}
+//key of our node's key-value pair in Firebase.
