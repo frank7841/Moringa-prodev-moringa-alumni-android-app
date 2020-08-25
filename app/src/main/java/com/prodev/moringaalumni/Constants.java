a070c2b2780646ac66058d6234b6ffaf5f433078
Njoki Kamau <njokifaith61@gmail.com>
2020-08-25 13:57:26 +0300

    change colors to moringa theme

--- app/src/main/java/com/prodev/moringaalumni/Constants.java
+++ app/src/main/java/com/prodev/moringaalumni/Constants.java
@@ -2,6 +2,6 @@ package com.prodev.moringaalumni;
 
 public class Constants {
 
-    public static final String FIREBASE_CHILD_SEARCHED_USER = "searchedUser";
+    public static final String FIREBASE_CHILD_SEARCHED_PROFILE = "searchedProfile";
 }
 //key of our node's key-value pair in Firebase.
--- app/src/main/java/com/prodev/moringaalumni/MainActivity.java
+++ app/src/main/java/com/prodev/moringaalumni/MainActivity.java
@@ -4,9 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,9 +11,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("message");
-
-   
-
 }
