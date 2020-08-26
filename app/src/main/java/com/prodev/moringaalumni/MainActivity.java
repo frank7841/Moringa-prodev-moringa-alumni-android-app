fff76100b23456194c1721dda2eb28d825d34124
Njoki Kamau <njokifaith61@gmail.com>
2020-08-26 19:05:48 +0300

    initial commit

--- app/src/main/java/com/prodev/moringaalumni/MainActivity.java
+++ app/src/main/java/com/prodev/moringaalumni/MainActivity.java
@@ -17,28 +17,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        int id = item.getItemId();
-        if (id==R.id.action_logout){
-            logout();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void logout(){
-        FirebaseAuth.getInstance().signOut();
-        Intent intent = new Intent(MainActivity.this,LoginActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-        finish();
-    }
 }
