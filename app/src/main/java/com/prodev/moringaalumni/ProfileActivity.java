29cc4432ae3ab2054ba1557022669fc44f962eae
Njoki Kamau <njokifaith61@gmail.com>
2020-08-27 01:09:26 +0300

    make profileactivity launcher

--- app/src/main/java/com/prodev/moringaalumni/ProfileActivity.java
+++ app/src/main/java/com/prodev/moringaalumni/ProfileActivity.java
@@ -3,9 +3,16 @@ package com.prodev.moringaalumni;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    //Firebase auth
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +21,28 @@ public class ProfileActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Profile");
+
+        //init
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    private void checkUserStatus() {
+        //get current user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            //user is signed in stay here
+        } else {
+            //user not signed in, go to main activity
+
+            startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+            finish();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        //check on start of app
+        checkUserStatus();
+        super.onStart();
     }
 }
