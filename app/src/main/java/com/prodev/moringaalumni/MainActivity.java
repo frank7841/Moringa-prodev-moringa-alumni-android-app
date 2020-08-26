6616824140fd2b512ed13a9ccb636d33055419a0
frank7841 <franklinewax@gmail.com>
2020-08-26 20:14:17 +0300

    Home button varriable declaration

--- app/src/main/java/com/prodev/moringaalumni/MainActivity.java
+++ app/src/main/java/com/prodev/moringaalumni/MainActivity.java
@@ -7,14 +7,21 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.register_btn) Button mregisterButton;
+    @BindView(R.id.login_btn) Button mloginbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
