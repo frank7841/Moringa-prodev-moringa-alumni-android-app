8c1555582f5a08430f13f7f980cb4f0db5c8d40d
Njoki Kamau <njokifaith61@gmail.com>
2020-08-26 20:45:51 +0300

    handled register button click in main activity

--- app/src/main/java/com/prodev/moringaalumni/MainActivity.java
+++ app/src/main/java/com/prodev/moringaalumni/MainActivity.java
@@ -7,14 +7,33 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    //views
+    Button mRegisterBtn, mLoginBtn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //init views
+        mRegisterBtn = findViewById(R.id.register_btn);
+        mLoginBtn = findViewById(R.id.login_btn);
+
+        //handle register btn click
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
+        });
     }
 }
