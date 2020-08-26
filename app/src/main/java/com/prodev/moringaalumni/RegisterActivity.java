d8991717f84b98add2a4bdbfaf403abd2e9037c8
Njoki Kamau <njokifaith61@gmail.com>
2020-08-26 22:19:27 +0300

    created register method which takes in email address and password to validate

--- app/src/main/java/com/prodev/moringaalumni/RegisterActivity.java
+++ app/src/main/java/com/prodev/moringaalumni/RegisterActivity.java
@@ -1,13 +1,23 @@
 package com.prodev.moringaalumni;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -17,6 +27,8 @@ public class RegisterActivity extends AppCompatActivity {
 
     //progressbar to display while registering user
     ProgressDialog progressDialog;
+    // Declare instance of firebase Auth
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +47,9 @@ public class RegisterActivity extends AppCompatActivity {
         mPasswordEt = findViewById(R.id.passwordEt);
         mRegisterBtn = findViewById(R.id.registerBtn);
 
+        //In the onCreate() method, initialize the FirebaseAuth instance
+        mAuth = FirebaseAuth.getInstance();
+
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Registering User...");
 
@@ -42,10 +57,47 @@ public class RegisterActivity extends AppCompatActivity {
         mRegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //input email,password
+                String email = mEmailEt.getText().toString().trim();
+                String password = mPasswordEt.getText().toString().trim();
+
+                //validate
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    mEmailEt.setError("Invalid Email");
+                    mEmailEt.setFocusable(true);
+                }
+                else if (password.length()<6){
+                    mPasswordEt.setError("Password length at least 6 characters");
+                    mPasswordEt.setFocusable(true);
+                }
+                else {
+
+                    registerUser(email, password); //register the user
+                }
 
             }
         });
     }
+
+    private void registerUser(String email, String password) {
+        //when email and password are valid progress dialog should be showing
+        progressDialog.show();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                        }
+
+
+                    }
 
     @Override
     public boolean onSupportNavigateUp(){
