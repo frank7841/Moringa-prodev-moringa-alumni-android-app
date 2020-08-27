ecc3a0dea94cfaa0978c55f69e6d17366e6214c2
frank7841 <franklinewax@gmail.com>
2020-08-27 14:31:45 +0300

    creating clasas  Modeluser constructor

--- app/src/main/java/com/prodev/moringaalumni/ModelUser.java
+++ app/src/main/java/com/prodev/moringaalumni/ModelUser.java
@@ -1,4 +1,17 @@
 package com.prodev.moringaalumni;
 
-public class ModelUsers {
+public class ModelUser {
+
+    String name, email, search, phone, image, cover;
+     public ModelUser(){
+     }
+
+    public ModelUser(String name, String email, String search, String phone, String image, String cover) {
+        this.name = name;
+        this.email = email;
+        this.search = search;
+        this.phone = phone;
+        this.image = image;
+        this.cover = cover;
+    }
 }
