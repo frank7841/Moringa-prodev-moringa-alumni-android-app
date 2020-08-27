fdc8c51becbd8912fc816100d7d3c483862d950f
lifereze <liferezearnold@gmail.com>
2020-08-27 04:26:23 +0300

    add database reference and hashmap to store user info in database realtime storage

--- app/src/main/java/com/prodev/moringaalumni/RegisterActivity.java
+++ app/src/main/java/com/prodev/moringaalumni/RegisterActivity.java
@@ -19,6 +19,10 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -96,6 +100,21 @@ public class RegisterActivity extends AppCompatActivity {
                             String uid = user.getUid();
                             //when a user is registred store user info in FB realtime database
                             //using hashmap
+                            HashMap<Object, String> hashMap = new HashMap<>();
+                            //put info in hashmap
+                            hashMap.put("email", email);
+                            hashMap.put("uid", email);
+                            hashMap.put("name", "");// baadae
+                            hashMap.put("phone", "");
+                            hashMap.put("image", "");
+
+                            // firebase instance
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            // path to store user data named "Users"
+                            DatabaseReference reference = database.getReference("Users");
+                            //put data within hashmaps in database
+                            reference.child(uid).setValue(hashMap);
+
 
                             Toast.makeText(RegisterActivity.this, "REgistered...\n"+user.getEmail(), Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(RegisterActivity.this, ProfileActivity.class));
