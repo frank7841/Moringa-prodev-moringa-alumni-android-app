1844524a02ddcb3426b52f68d5dacb51295d4c00
frank7841 <franklinewax@gmail.com>
2020-08-27 13:44:02 +0300

    inflating usersferagment layout

--- app/src/main/java/com/prodev/moringaalumni/UsersFragment.java
+++ app/src/main/java/com/prodev/moringaalumni/UsersFragment.java
@@ -59,6 +59,7 @@ public class UsersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users, container, false);
+        View view = inflater.inflate(R.layout.fragment_users, container, false);
+        return view;
     }
 }
