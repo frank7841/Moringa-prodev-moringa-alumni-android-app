55c8dc94351951fbf05f9a8c47d37b75cb0f5bca
lifereze <liferezearnold@gmail.com>
2020-08-27 07:28:25 +0300

    add jupiter dependency for androidtest

---
+++ app/src/androidTest/java/com/prodev/moringaalumni/DashboardActivityTest.java
@@ -0,0 +1,10 @@
+package com.prodev.moringaalumni;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DashboardActivityTest {
+
+    @org.junit.jupiter.api.Test
+    void onCreate() {
+    }
+}
