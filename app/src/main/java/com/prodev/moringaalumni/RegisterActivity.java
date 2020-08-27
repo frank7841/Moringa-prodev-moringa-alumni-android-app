83f329f1a766efda9d807b84d7dfdf63220b1288
lifereze <liferezearnold@gmail.com>
2020-08-27 11:13:08 +0300

    resolved merge conflicts

--- app/src/main/java/com/prodev/moringaalumni/RegisterActivity.java
+++ app/src/main/java/com/prodev/moringaalumni/RegisterActivity.java
@@ -103,7 +103,7 @@ public class RegisterActivity extends AppCompatActivity {
                             progressDialog.dismiss();
                             FirebaseUser user = mAuth.getCurrentUser();
                             Toast.makeText(RegisterActivity.this, "Registered...\n"+user.getEmail(), Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(RegisterActivity.this, ProfileActivity.class));
+                            startActivity(new Intent(RegisterActivity.this, DashboardActivity.class));
                             finish();
                         } else {
                             // If sign in fails, display a message to the user.
