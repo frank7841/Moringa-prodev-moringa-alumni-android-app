cc03c70e4bbc30793a7cbb0236f83572491ddc54
lifereze <liferezearnold@gmail.com>
2020-08-27 06:06:03 +0300

    add navigation listeners to the oncreate method

--- app/src/main/java/com/prodev/moringaalumni/DashboardActivity.java
+++ app/src/main/java/com/prodev/moringaalumni/DashboardActivity.java
@@ -3,16 +3,50 @@ package com.prodev.moringaalumni;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class DashboardActivity extends AppCompatActivity {
+    // firebase auth
+    FirebaseAuth firebaseAuth;
+
+    ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        ActionBar actionBar = getSupportActionBar();
+        //Action bar and its title
+        actionBar = getSupportActionBar();
         actionBar.setTitle("Profile");
+
+        //init
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //bottom navigation
+        BottomNavigationView navigationView = findViewById(R.id.navigation);
+        navigationView.setOnNavigationItemSelectedListener();
+
+    }
+
+    private void checkUserStatus(){
+//        get current user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user !=null){
+            // user is signed in stay here
+            // set email of logged in user
+             mProfileTv.setText(user.getEmail());
+
+        }
+        else {
+            //user not signed in, go to main activity
+            startActivity(new Intent(DashboardActivity.this, MainActivity.class));
+            finish();
+        }
     }
 }
