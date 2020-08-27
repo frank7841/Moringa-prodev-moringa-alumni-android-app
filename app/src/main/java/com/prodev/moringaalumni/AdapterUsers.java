6577ad63a722b41808acaaad5256f9fc2e22b951
frank7841 <franklinewax@gmail.com>
2020-08-27 14:56:22 +0300

    Creating constructor for AdapterUsers class

--- app/src/main/java/com/prodev/moringaalumni/AdapterUsers.java
+++ app/src/main/java/com/prodev/moringaalumni/AdapterUsers.java
@@ -1,4 +1,49 @@
 package com.prodev.moringaalumni;
 
-public class AdapterUsers {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class AdapterUsers extends RecyclerView.Adapter<AdapterUsers.MyHolder> {
+    Context context;
+    List <ModelUser>userList;
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    //View holder class
+    class MyHolder extends RecyclerView.ViewHolder{
+        ImageView mAvatarIv;
+        TextView mNameTv, mEmailTv;
+
+
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+
+            //init views
+            mAvatarIv= itemView.findViewById(R.id.avartarIv);
+            mNameTv= itemView.findViewById(R.id.nameTv);
+            mEmailTv= itemView.findViewById(R.id.emailTv);
+        }
+    }
 }
