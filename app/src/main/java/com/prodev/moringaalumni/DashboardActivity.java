32e4b9337cfdbeb2cfd59ec2c51c3e75bd90bb04
lifereze <liferezearnold@gmail.com>
2020-08-27 07:13:27 +0300

    add the bottom nav bar

--- app/src/main/java/com/prodev/moringaalumni/DashboardActivity.java
+++ app/src/main/java/com/prodev/moringaalumni/DashboardActivity.java
@@ -3,6 +3,7 @@ package com.prodev.moringaalumni;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class DashboardActivity extends AppCompatActivity {
     // firebase auth
+
     FirebaseAuth firebaseAuth;
 
     ActionBar actionBar;
@@ -34,6 +36,13 @@ public class DashboardActivity extends AppCompatActivity {
         BottomNavigationView navigationView = findViewById(R.id.navigation);
         navigationView.setOnNavigationItemSelectedListener(selectedListener);
 
+        //home fragment transaction default
+        actionBar.setTitle("Home");// change actionbar title
+        HomeFragment fragment1 = new HomeFragment();
+        FragmentTransaction ft1 = getSupportFragmentManager().beginTransaction();
+        ft1.replace(R.id.content, fragment1, "");
+        ft1.commit();
+
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener selectedListener=
@@ -44,12 +53,27 @@ public class DashboardActivity extends AppCompatActivity {
                     switch (menuItem.getItemId()){
                         case R.id.nav_home:
                             //home fragment transaction
+                            actionBar.setTitle("Home");// change actionbar title
+                            HomeFragment fragment1 = new HomeFragment();
+                            FragmentTransaction ft1 = getSupportFragmentManager().beginTransaction();
+                            ft1.replace(R.id.content, fragment1, "");
+                            ft1.commit();
                             return true;
                         case R.id.nav_profile:
                             //profile fragment transaction
+                            actionBar.setTitle("Profile");// change actionbar title
+                            ProfileFragment fragment2 = new ProfileFragment();
+                            FragmentTransaction ft2 = getSupportFragmentManager().beginTransaction();
+                            ft2.replace(R.id.content, fragment2, "");
+                            ft2.commit();
                             return true;
                         case R.id.nav_users:
                             //usres fragment transaction
+                            actionBar.setTitle("Users");// change actionbar title
+                            UsersFragment fragment3 = new UsersFragment();
+                            FragmentTransaction ft3 = getSupportFragmentManager().beginTransaction();
+                            ft3.replace(R.id.content, fragment3, "");
+                            ft3.commit();
                             return true;
                     }
                     return false;
@@ -62,7 +86,7 @@ public class DashboardActivity extends AppCompatActivity {
         if (user !=null){
             // user is signed in stay here
             // set email of logged in user
-             mProfileTv.setText(user.getEmail());
+             //mProfileTv.setText(user.getEmail());
 
         }
         else {
