f519083f474483b6ad67c294a12961032e0873f0
frank7841 <franklinewax@gmail.com>
2020-08-28 13:04:15 +0300

    generated constructor, getter and setter methods

--- app/src/main/java/com/prodev/moringaalumni/ModelChat.java
+++ app/src/main/java/com/prodev/moringaalumni/ModelChat.java
@@ -1,4 +1,47 @@
 package com.prodev.moringaalumni;
 
 public class ModelChat {
+    String message,sender, receiver, timestamp;
+
+    public ModelChat() {
+    }
+
+    public ModelChat(String message, String sender, String receiver, String timestamp) {
+        this.message = message;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
 }
