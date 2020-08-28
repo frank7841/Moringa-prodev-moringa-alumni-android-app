8d60c442632133754d5a88bc4be50cfb6ef35bb7
lifereze <liferezearnold@gmail.com>
2020-08-28 05:47:32 +0300

    refactor google sign-in to display user info in the profile section

--- app/src/main/java/com/prodev/moringaalumni/LoginActivity.java
+++ app/src/main/java/com/prodev/moringaalumni/LoginActivity.java
@@ -34,6 +34,10 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class LoginActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 100 ;
@@ -278,6 +282,31 @@ private FirebaseAuth mAuth;
                             // Sign in success, update UI with the signed-in user's information
 
                             FirebaseUser user = mAuth.getCurrentUser();
+                            //if a user is signig for the first time, display google info
+                            if (task.getResult().getAdditionalUserInfo().isNewUser()){
+
+                                //get user email and uid from auth
+                                String email = user.getEmail();
+                                String uid = user.getUid();
+                                //when a user is registred store user info in FB realtime database
+                                //using hashmap
+                                HashMap<Object, String> hashMap = new HashMap<>();
+                                //put info in hashmap
+                                hashMap.put("email", email);
+                                hashMap.put("uid", email);
+                                hashMap.put("name", "");// baadae
+                                hashMap.put("phone", "");
+                                hashMap.put("image", "");
+                                hashMap.put("cover", "");
+
+                                // firebase instance
+                                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                // path to store user data named "Users"
+                                DatabaseReference reference = database.getReference("Users");
+                                //put data within hashmaps in database
+                                reference.child(uid).setValue(hashMap);
+                            }
+
                            // updateUI(user);
                             //show user email in toast
                             Toast.makeText(LoginActivity.this, ""+user.getEmail(),Toast.LENGTH_SHORT).show();
