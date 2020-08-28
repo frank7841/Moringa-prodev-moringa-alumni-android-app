1073545e364235454493280c50f94ba33268e312
frank7841 <franklinewax@gmail.com>
2020-08-28 13:09:40 +0300

    MyHolder class extending Recyclerview

--- app/src/main/java/com/prodev/moringaalumni/AdapterChat.java
+++ app/src/main/java/com/prodev/moringaalumni/AdapterChat.java
@@ -1,4 +1,18 @@
 package com.prodev.moringaalumni;
 
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class AdapterChat {
+
+    //View holder class
+
+    class MyHolder extends RecyclerView.ViewHolder{
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
