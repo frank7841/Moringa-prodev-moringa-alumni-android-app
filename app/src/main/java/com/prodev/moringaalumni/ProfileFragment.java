97b5bab8a511b8c8ee0629f673eaac182ad15fcf
lifereze <liferezearnold@gmail.com>
2020-08-28 13:25:47 +0300

    polish the layout file

--- app/src/main/java/com/prodev/moringaalumni/ProfileFragment.java
+++ app/src/main/java/com/prodev/moringaalumni/ProfileFragment.java
@@ -389,7 +389,6 @@ public class ProfileFragment extends Fragment {
             }
             break;
         }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     @Override
