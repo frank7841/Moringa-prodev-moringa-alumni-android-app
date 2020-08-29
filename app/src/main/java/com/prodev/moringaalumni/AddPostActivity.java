74488e39b0263ed1aaeaf4ce267a985e1f5c37e8
lifereze <liferezearnold@gmail.com>
2020-08-29 12:59:42 +0300

    initialize action bar in the add post activity

--- app/src/main/java/com/prodev/moringaalumni/AddPostActivity.java
+++ app/src/main/java/com/prodev/moringaalumni/AddPostActivity.java
@@ -1,5 +1,6 @@
 package com.prodev.moringaalumni;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,9 @@ public class AddPostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_post);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Add New Post");
+        // enable back button in action bar
     }
 }
--- app/src/main/java/com/prodev/moringaalumni/ChatListFragment.java
+++ app/src/main/java/com/prodev/moringaalumni/ChatListFragment.java
@@ -2,59 +2,28 @@ package com.prodev.moringaalumni;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.core.view.MenuItemCompat;
-import androidx.fragment.app.Fragment;
-
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.SearchView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ChatListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ChatListFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    // firebase auth
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    FirebaseAuth firebaseAuth;
+
 
     public ChatListFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChatFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ChatListFragment newInstance(String param1, String param2) {
-        ChatListFragment fragment = new ChatListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
 
