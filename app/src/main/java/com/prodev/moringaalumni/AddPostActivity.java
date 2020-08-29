8ccd2445685d86aaa2ad28a9d1f7cae8c58e6244
lifereze <liferezearnold@gmail.com>
2020-08-29 13:26:55 +0300

    hide post action activity from the main activity

--- app/src/main/java/com/prodev/moringaalumni/AddPostActivity.java
+++ app/src/main/java/com/prodev/moringaalumni/AddPostActivity.java
@@ -1,11 +1,16 @@
 package com.prodev.moringaalumni;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class AddPostActivity extends AppCompatActivity {
+
+    ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +28,19 @@ public class AddPostActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         onBackPressed();//go to previous activity
         return super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+
+        menu.findItem(R.id.action_add_post).setVisible(false);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
