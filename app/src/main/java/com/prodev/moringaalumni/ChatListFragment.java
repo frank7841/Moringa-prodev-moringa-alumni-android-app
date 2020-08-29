71f1302462279ae94138ac0e4f29e98beb907c20
lifereze <liferezearnold@gmail.com>
2020-08-29 12:47:52 +0300

    initialize firebase in the chatlist fragment

--- app/src/main/java/com/prodev/moringaalumni/ChatListFragment.java
+++ app/src/main/java/com/prodev/moringaalumni/ChatListFragment.java
@@ -1,12 +1,23 @@
 package com.prodev.moringaalumni;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -46,19 +57,62 @@ public class ChatListFragment extends Fragment {
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        //init
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        return view;
+    }
+
+    private void checkUserStatus(){
+//        get current user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user !=null){
+            // user is signed in stay here
+            // set email of logged in user
+            //mProfileTv.setText(user.getEmail());
+
+        }
+        else {
+            //user not signed in, go to main activity
+            startActivity(new Intent(getActivity(), MainActivity.class));
+            getActivity().finish();
+        }
+    }
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState){
+        setHasOptionsMenu(true); //to show menu options
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public void onCreateOptionsMenu (Menu menu, MenuInflater inflater){
+        inflater.inflate(R.menu.menu_main, menu);
+
+        // hide post action from this fragment
+        menu.findItem(R.id.action_add_post).setVisible(false);
+
+        super.onCreateOptionsMenu(menu, inflater);
+
+    }
+
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_logout){
+            firebaseAuth.signOut();
+            checkUserStatus();
+        }
+
+        return super.onOptionsItemSelected(item);
+
     }
 }
