9e3930daf2fad8d4e0fd99465923db3d5c09587e
frank7841 <franklinewax@gmail.com>
2020-08-31 20:30:52 +0300

    Checking to make sure current user is not displayed on the alumni

--- app/src/main/java/com/prodev/moringaalumni/UsersFragment.java
+++ app/src/main/java/com/prodev/moringaalumni/UsersFragment.java
@@ -47,13 +47,45 @@ public class UsersFragment extends Fragment {
     FirebaseAuth firebaseAuth;
 
 
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
 
     public UsersFragment() {
         // Required empty public constructor
     }
 
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment UsersFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static UsersFragment newInstance(String param1, String param2) {
+        UsersFragment fragment = new UsersFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
-
+   /* @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }*/
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -65,7 +97,7 @@ public class UsersFragment extends Fragment {
         firebaseAuth = FirebaseAuth.getInstance();
         recyclerView = view.findViewById(R.id.users_recyclerView);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         //initialising userList
         userList = new ArrayList<>();
@@ -84,12 +116,17 @@ public class UsersFragment extends Fragment {
                 userList.clear();
                 for (DataSnapshot ds: snapshot.getChildren()){
                     ModelUser modelUser = ds.getValue(ModelUser.class);
-                    if (!modelUser.getUid().equals(fUser.getUid())){
+                    if (!fUser.getUid().equals(modelUser.getUid())){
                         userList.add(modelUser);
                     }
+//
+//                     if(!fUser.getUid().equals(ModelUser.getUid())){
+//                        userList.add(modelUser);
+//                    }
                     //adapter
                     adapterUsers = new AdapterUsers(getActivity(),userList);
                     //setting adapter to recyclerView
+                    recyclerView.setAdapter(adapterUsers);
 
                 }
             }
@@ -118,7 +155,7 @@ public class UsersFragment extends Fragment {
                      */
                     if (!modelUser.getUid().equals(fUser.getUid())){
                         if(modelUser.getName().toLowerCase().contains(query.toLowerCase()) ||
-                        modelUser.getEmail().toLowerCase().contains(query.toLowerCase())){
+                                modelUser.getEmail().toLowerCase().contains(query.toLowerCase())){
                             userList.add(modelUser);
                         }
 
