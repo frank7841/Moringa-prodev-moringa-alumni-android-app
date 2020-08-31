3dddf21714b470e175a2a9953c3b694c44f7f25a
lifereze <liferezearnold@gmail.com>
2020-08-31 16:26:29 +0300

    add recycler view and its properties

--- app/src/main/java/com/prodev/moringaalumni/HomeFragment.java
+++ app/src/main/java/com/prodev/moringaalumni/HomeFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -51,6 +52,13 @@ public class HomeFragment extends Fragment {
 
         //init
         firebaseAuth = FirebaseAuth.getInstance();
+
+        // recycler view and its properties
+        recyclerView = view.findViewById(R.id.postsRecyclerview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        //show newest post
+        layoutManager.setStackFromEnd(true);
+        layoutManager.setReverseLayout(true);
       return view;
     }
     private void checkUserStatus(){
