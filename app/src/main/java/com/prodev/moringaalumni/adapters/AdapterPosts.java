461732f3c4497b79f7d06f977a803fcec26c1e1d
lifereze <liferezearnold@gmail.com>
2020-08-31 14:16:58 +0300

    initialize views in the adapetr class

--- app/src/main/java/com/prodev/moringaalumni/adapters/AdapterPosts.java
+++ app/src/main/java/com/prodev/moringaalumni/adapters/AdapterPosts.java
@@ -1,4 +1,33 @@
 package com.prodev.moringaalumni.adapters;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.prodev.moringaalumni.R;
+
 public class AdapterPosts {
+
+    //view holder class
+    class MyHolder extends RecyclerView.ViewHolder{
+
+        //views from row_post.xml
+        ImageView uPictureIv, pImageIv;
+        TextView uNametv, pTimeTv, pDescriptionTv, pLikesTv;
+        ImageButton moreBtn;
+        Button likeBtn, commentBtn, shareBtn;
+
+        public MyHolder(@NonNull View itemView){
+            super(itemView);
+
+            //init views
+            uPictureIv = itemView.findViewById(R.id.uPictureIv);
+
+        }
+    }
 }
