c6b52c8d27cb64402b86aea48309de252877b110
lifereze <liferezearnold@gmail.com>
2020-08-31 12:34:12 +0300

    generate a construtor ModelPost

--- app/src/main/java/com/prodev/moringaalumni/models/ModelPost.java
+++ app/src/main/java/com/prodev/moringaalumni/models/ModelPost.java
@@ -1,4 +1,21 @@
 package com.prodev.moringaalumni.models;
 
 public class ModelPost {
+    // use same name as uploading post
+    String pId, pTitle, pDescr, pImage, uid, uEmail, uDp, uName;
+
+    public ModelPost(){
+
+    }
+
+    public ModelPost(String pId, String pTitle, String pDescr, String pImage, String uid, String uEmail, String uDp, String uName) {
+        this.pId = pId;
+        this.pTitle = pTitle;
+        this.pDescr = pDescr;
+        this.pImage = pImage;
+        this.uid = uid;
+        this.uEmail = uEmail;
+        this.uDp = uDp;
+        this.uName = uName;
+    }
 }
