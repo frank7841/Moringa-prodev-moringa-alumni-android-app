020c5fb665bf5972c4c9ca8693db85dcf98f038c
lifereze <liferezearnold@gmail.com>
2020-08-31 12:36:08 +0300

    generate getters and setters of the model class

--- app/src/main/java/com/prodev/moringaalumni/models/ModelPost.java
+++ app/src/main/java/com/prodev/moringaalumni/models/ModelPost.java
@@ -18,4 +18,68 @@ public class ModelPost {
         this.uDp = uDp;
         this.uName = uName;
     }
+
+    public String getpId() {
+        return pId;
+    }
+
+    public void setpId(String pId) {
+        this.pId = pId;
+    }
+
+    public String getpTitle() {
+        return pTitle;
+    }
+
+    public void setpTitle(String pTitle) {
+        this.pTitle = pTitle;
+    }
+
+    public String getpDescr() {
+        return pDescr;
+    }
+
+    public void setpDescr(String pDescr) {
+        this.pDescr = pDescr;
+    }
+
+    public String getpImage() {
+        return pImage;
+    }
+
+    public void setpImage(String pImage) {
+        this.pImage = pImage;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getuEmail() {
+        return uEmail;
+    }
+
+    public void setuEmail(String uEmail) {
+        this.uEmail = uEmail;
+    }
+
+    public String getuDp() {
+        return uDp;
+    }
+
+    public void setuDp(String uDp) {
+        this.uDp = uDp;
+    }
+
+    public String getuName() {
+        return uName;
+    }
+
+    public void setuName(String uName) {
+        this.uName = uName;
+    }
 }
