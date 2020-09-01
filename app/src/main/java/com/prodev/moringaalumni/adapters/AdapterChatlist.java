3697993d701acce2c0f9199ccea16f4bfe360ac8
frank7841 <franklinewax@gmail.com>
2020-09-01 06:39:26 +0300

    defining AdapterChatlist class extending RecyclerView

--- app/src/main/java/com/prodev/moringaalumni/adapters/AdapterChatlist.java
+++ app/src/main/java/com/prodev/moringaalumni/adapters/AdapterChatlist.java
@@ -1,6 +1,7 @@
 package com.prodev.moringaalumni.adapters;
 
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -9,8 +10,24 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.prodev.moringaalumni.R;
 
-public class AdapterChatlist {
-     class MyHolder extends RecyclerView.ViewHolder{
+public class AdapterChatlist extends  RecyclerView.Adapter<AdapterChatlist.MyHolder> {
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class MyHolder extends RecyclerView.ViewHolder{
          ImageView profileIv,onlineStatusIv;
          TextView nameTv,lastMessageTv;
 
