8f5087f3b057eb181ab3e4b7f0ee9c8c3f31142e
frank7841 <franklinewax@gmail.com>
2020-09-01 06:37:16 +0300

    declaring views for row_chatlist

--- app/src/main/java/com/prodev/moringaalumni/adapters/AdapterChatlist.java
+++ app/src/main/java/com/prodev/moringaalumni/adapters/AdapterChatlist.java
@@ -1,15 +1,26 @@
 package com.prodev.moringaalumni.adapters;
 
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.prodev.moringaalumni.R;
+
 public class AdapterChatlist {
      class MyHolder extends RecyclerView.ViewHolder{
+         ImageView profileIv,onlineStatusIv;
+         TextView nameTv,lastMessageTv;
 
          public MyHolder(@NonNull View itemView) {
              super(itemView);
+             profileIv = itemView.findViewById(R.id.proifleIv);
+             onlineStatusIv = itemView.findViewById(R.id.onlineStatusIv);
+             nameTv = itemView.findViewById(R.id.nameTv);
+             lastMessageTv = itemView.findViewById(R.id.lastMessageTv);
+
          }
      }
 }
