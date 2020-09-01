2e68b794d25e5bbe3c450cd85eb2666fec2d4ed5
frank7841 <franklinewax@gmail.com>
2020-09-01 06:33:15 +0300

    Creating Myholder class

--- app/src/main/java/com/prodev/moringaalumni/adapters/AdapterChatlist.java
+++ app/src/main/java/com/prodev/moringaalumni/adapters/AdapterChatlist.java
@@ -1,4 +1,15 @@
 package com.prodev.moringaalumni.adapters;
 
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class AdapterChatlist {
+     class MyHolder extends RecyclerView.ViewHolder{
+
+         public MyHolder(@NonNull View itemView) {
+             super(itemView);
+         }
+     }
 }
