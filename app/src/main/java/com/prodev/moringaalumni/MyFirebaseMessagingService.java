64a24f0909a1501c040bb6e760ec5590a2e51d16
lifereze <liferezearnold@gmail.com>
2020-09-09 18:45:06 +0300

    notification implemented

---
+++ app/src/main/java/com/prodev/moringaalumni/MyFirebaseMessagingService.java
@@ -0,0 +1,33 @@
+package com.prodev.moringaalumni;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.media.RingtoneManager;
+
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    //init remote message
+   RemoteMessage remoteMessage;
+    @Override
+    public void onMessageReceived(RemoteMessage message) {
+        super.onMessageReceived(remoteMessage);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "channel_id")
+                .setContentTitle(remoteMessage.getNotification().getTitle())
+                .setContentText(remoteMessage.getNotification().getBody())
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setStyle(new NotificationCompat.BigTextStyle())
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setAutoCancel(true);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}
